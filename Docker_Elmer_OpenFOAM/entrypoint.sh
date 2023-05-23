b98ae9a35db3504f7b3630af80b410a4a37928ed
Arved Wintzer <arved.wintzer@ikz-berlin.de>
2023-05-23 18:21:46 +0200

    update Elmer, OpenFOAM version

--- Docker_Elmer_OpenFOAM/entrypoint.sh
+++ Docker_Elmer_OpenFOAM/entrypoint.sh
@@ -12,7 +12,11 @@ export HOME=/home/user
 echo 'bind '"'"'"\e[A": history-search-backward'"'" >> $HOME/.bashrc 
 echo 'bind '"'"'"\e[B": history-search-forward'"'" >> $HOME/.bashrc 
 echo 'LS_COLORS=$LS_COLORS:'"'"'di=32:'"'"' ; export LS_COLORS' >> $HOME/.bashrc 
-echo "alias of21='source /usr/lib/openfoam/openfoam2112/etc/bashrc '" >> $HOME/.bashrc
+echo "alias of2212='source /usr/lib/openfoam/openfoam2212/etc/bashrc '" >> $HOME/.bashrc
+echo "alias of2112='source /usr/lib/openfoam/openfoam2112/etc/bashrc '" >> $HOME/.bashrc
+echo "alias mc='source /usr/lib/mc/mc-wrapper.sh'" >> $HOME/.bashrc
+echo "echo "starting openfoam2212"" >> $HOME/.bashrc
+echo "of2212" >> $HOME/.bashrc
 
 # OpenFOAM enviroment variables
 export WM_USER_PROJECT_DIR=/home/workdir/user-foam
